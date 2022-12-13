0d3d36223375e51586cbe168ae9ed9a66042477a
Wesley <wesley.machado.2018@polytechnique.org>
2022-12-13 18:39:33 +0100

    Implemented Bank listing of Clients with Stream API

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Collections;
 //import java.util.Stream;
 import java.lang.Integer;
 
@@ -17,6 +18,10 @@ class Bank {
 	public float getBalance() {
 		return this.balance;
 	}
+	@Override
+	public String toString(){
+		return this.name;
+	}
 }
 
 class Client{
@@ -29,6 +34,10 @@ class Client{
 	}
 	public List<Bank> getBanks() {
 		return this.banks;
+	}
+	@Override
+	public String toString(){
+		return this.name;
 	}
 }
 
@@ -46,7 +55,7 @@ public class Test {
 
 		banksC.add(new Bank("CA", 600));
 
-		banksD.add(new Bank("SG", 700));
+		banksD.add(new Bank("SG", 100));
 
 		LinkedList<Client> clients = new LinkedList<Client>();
 		clients.add(new Client("A", banksA));
@@ -56,15 +65,38 @@ public class Test {
 
 		System.out.println(clients.stream()
 			.filter(client -> 
-				client.getBanks().stream()
-					.mapToDouble(Bank::getBalance)
-				/*
 				((Client) client).banks.stream()
 					.mapToDouble(bank -> ((Bank) bank).balance)
-				*/
 					.sum() < 600)
 			.map(client -> client.name)
 			.collect(Collectors.toList())
 			);
+		System.out.println(clients.stream()
+			.map(client -> 
+				((Client) client).banks.stream()
+					.mapToDouble(bank -> ((Bank) bank).balance)
+					.sum())
+			.reduce(0d, (total, lastSum) -> total + lastSum)
+			);
+
+		Map<String, LinkedList<Client>> groupByBank
+			= new HashMap<String, LinkedList<Client>>();
+
+		clients.stream()
+				.forEach(client ->
+						((Client) client).banks.stream()
+						.forEach(bank -> {
+							LinkedList<Client> temp
+								= new LinkedList<Client>();
+							temp.add(client);
+							groupByBank.merge(bank.name,
+								temp,
+								(oldValue, newValue) -> {
+									oldValue.addAll(newValue);
+									return oldValue;
+								});
+						})
+				);
+		System.out.println(groupByBank);
 	}
 }
